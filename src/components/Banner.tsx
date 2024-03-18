cbb6ecfa4a6be787de20e51c90e0dc536ea4d1d3
ElenaiDev <ec22761@qmul.ac.uk>
2024-03-18 23:28:41 +0000

    Add Banner component and update title in index.html

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+function Banner() {
+    return (
+      <section className='banner'>
+        <article>
+            <h1>Hi, I'm Roman Pretty</h1>
+            <p>I'm a Computer Science Demonstrator at Queen Mary University of London (QMUL) with a multifaceted background in software development. My journey is characterized by a passion for teaching, creativity, and leveraging technology to engage and inspire.</p>
+        </article>
+        {/* <img src={portrait} alt='Roman Pretty' id='portrait'></img> */}
+      </section>
+    )
+  }
+  
+  export default Banner
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
 import logo from '../assets/logo.png';
+import React, { useState } from 'react';
 
 function Header() {
+
+    const [selected, setSelected] = useState('home');
+    let selectedValue = ['selected', '', '', ''];
+
+    if (selected === 'home') {
+        selectedValue = ['selected', '', '', ''];
+    } else if (selected === 'about') {
+        selectedValue = ['', 'selected', '', ''];
+    } else if (selected === 'projects') {
+        selectedValue = ['', '', 'selected', ''];
+    } else if (selected === 'resume') {
+        selectedValue = ['', '', '', 'selected'];
+    }
+
     return (
-      <header>
+        <header>
             <img src={logo} alt='Roman Pretty'></img>
             <nav>
                 <ul>
-                    <li className='selected'><a href="#home">Home</a><hr></hr></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#resume">Resume</a></li>
+                    <li className={selectedValue[0]}><a href="#home" onClick={() => { setSelected('home') }}>Home</a>{selected === 'home' || selected === '' ? <hr /> : null}</li>
+                    <li className={selectedValue[1]}><a href="#about" onClick={() => { setSelected('about') }}>About</a>{ selected === 'about' ? <hr/> : null }</li>
+                    <li className={selectedValue[2]}><a href="#projects" onClick={() => { setSelected('projects') }}>Projects</a>{ selected === 'projects' ? <hr/> : null }</li>
+                    <li className={selectedValue[3]}><a href="#resume" onClick={() => { setSelected('resume') }}>Resume</a>{ selected === 'resume' ? <hr/> : null }</li>
                 </ul>
             </nav>
             <button>Contact Me</button>
         </header>
     )
-  }
-  
-  export default Header
+}
+
+export default Header
