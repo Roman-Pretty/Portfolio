8704c5f70f7167516bdbb39d82920e708c3096e2
ec22761 <ec22761@qmul.ac.uk>
2024-03-19 14:54:46 +0000

    Refactor header component and update logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,43 @@
 import logo from '../assets/logo.png';
-import React, { useState } from 'react';
+import { FC, useState } from 'react';
+
+interface ListItemProps {
+    name: string;
+    isSelected: boolean;
+    href: string;
+    click: () => void;
+}
+
+const ListItem: FC<ListItemProps> = ({ name, isSelected, href, click }) => {
+    return (
+        <li className={isSelected ? 'selected' : ''}>
+            <a href={href} onClick={click}>{name}</a>
+            {isSelected && <hr />}
+        </li>
+    );
+};
 
 function Header() {
-
     const [selected, setSelected] = useState('home');
-    let selectedValue = ['selected', '', '', ''];
 
-    if (selected === 'home') {
-        selectedValue = ['selected', '', '', ''];
-    } else if (selected === 'about') {
-        selectedValue = ['', 'selected', '', ''];
-    } else if (selected === 'projects') {
-        selectedValue = ['', '', 'selected', ''];
-    } else if (selected === 'resume') {
-        selectedValue = ['', '', '', 'selected'];
-    }
+    const handleItemClick = (itemName: string) => {
+        setSelected(itemName);
+    };
 
     return (
         <header>
             <img src={logo} alt='Roman Pretty'></img>
             <nav>
                 <ul>
-                    <li className={selectedValue[0]}><a href="#home" onClick={() => { setSelected('home') }}>Home</a>{selected === 'home' || selected === '' ? <hr /> : null}</li>
-                    <li className={selectedValue[1]}><a href="#about" onClick={() => { setSelected('about') }}>About</a>{ selected === 'about' ? <hr/> : null }</li>
-                    <li className={selectedValue[2]}><a href="#projects" onClick={() => { setSelected('projects') }}>Projects</a>{ selected === 'projects' ? <hr/> : null }</li>
-                    <li className={selectedValue[3]}><a href="#resume" onClick={() => { setSelected('resume') }}>Resume</a>{ selected === 'resume' ? <hr/> : null }</li>
+                    <ListItem name='Home' isSelected={selected === 'home'} href='#home' click={() => handleItemClick('home')} />
+                    <ListItem name='About' isSelected={selected === 'about'} href='#about' click={() => handleItemClick('about')} />
+                    <ListItem name='Projects' isSelected={selected === 'projects'} href='#projects' click={() => handleItemClick('projects')} />
+                    <ListItem name='Resume' isSelected={selected === 'resume'} href='#resume' click={() => handleItemClick('resume')} />
                 </ul>
             </nav>
             <button>Contact Me</button>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
