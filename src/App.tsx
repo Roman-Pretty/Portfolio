0c5266b6e89bfe18e06f3043c9bf02093b43eb0d
ec22761 <ec22761@qmul.ac.uk>
2024-03-24 12:41:16 +0000

    Refactor code and add Chakra UI dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import Banner from './components/Banner'
-import Header from './components/Header'
 import './css/App.css'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Banner />
     </>
   )
 }
